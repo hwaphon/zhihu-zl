4e6ee49348e371c652d9ffcfd4a49c3d59615ba6
gaohuafeng <ghwaphon@gmail.com>
2021-03-10 22:11:37 +0800

    feat: 代码初试化

---
+++ src/App.svelte
@@ -0,0 +1,38 @@
+<script>
+	export let name;
+	let a = {
+		b: 2
+	}
+
+	const handleClick = () => {
+		console.log('handleClick');
+		a.b = Math.random();
+	}
+</script>
+
+<main>
+	<h1 on:click={ handleClick }>Hello {name}!</h1>
+	<p>{ a.b }</p>
+</main>
+
+<style>
+main {
+  text-align: center;
+  padding: 1em;
+  max-width: 240px;
+  margin: 0 auto;
+}
+
+h1 {
+  color: #ff3e00;
+  text-transform: uppercase;
+  font-size: 4em;
+  font-weight: 100;
+}
+
+@media (min-width: 640px) {
+  main {
+    max-width: none;
+  }
+}
+</style>
